e3d8ed2b6973b129bf0c1762db270d93dbde1cc0
Zou Jian <chsword@126.com>
2016-01-06 18:53:04 +0800

    1

--- src/WACG/CommandParameterHelper.cs
+++ src/WACG/CommandParameterHelper.cs
@@ -1,3 +1,8 @@
+
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+
 namespace WACG
 {
     public class CommandParameterHelper
@@ -5,6 +10,16 @@ namespace WACG
         public  static ParameterConatiner<T> Convert<T>(string[] args) where T : new()
         {
             var result = new T();
+            var props = typeof (T).GetTypeInfo()
+                .DeclaredProperties.Where(c => c.GetMethod.IsPublic
+                                               &&
+                                               !c.GetMethod.IsStatic).ToArray();
+            foreach (var propertyInfo in props)
+            {
+                var attrs = propertyInfo.GetCustomAttributes().Where(c => c is CommandParameterAttribute).ToArray();
+                Contract.Assert(attrs.Length <= 1);
+                if (!attrs.Any()) continue;
+            }
             return new ParameterConatiner<T>();
         }
     }
