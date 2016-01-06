596230dd5cc098fdb034e37d26872c7431a78942
Zou Jian <chsword@126.com>
2016-01-06 17:57:54 +0800

    1

---
+++ src/WACG/CommandParameterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WACG
+{
+    public class CommandParameterAttribute : Attribute
+    {
+        public CommandParameterAttribute()
+        {
+        }
+        public CommandParameterAttribute(string shortName)
+        {
+            ShortName = shortName;
+        }
+
+        public string ShortName { get; set; }
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// 当做为默认参数时参数顺序
+        /// </summary>
+        public int DefaultOrder { get; set; }
+
+    }
+}
